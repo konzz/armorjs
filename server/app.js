5e25fece0d8b7e9207b88e13c21f8babe97242f2
acasado <acasadotorres@gmail.com>
2014-02-12 12:40:10 -0500

    configured static files path and renamed the styles folder to css

--- server/app.js
+++ server/app.js
@@ -8,6 +8,5 @@ app.listen(8080);
 app.get('/', function(req, res){
   res.sendfile(__dirname + '/index.html');
 });
-
-app.use('/js', express.static(__dirname + '/client/js'));
-app.use('/css', express.static(__dirname + '/client/css'));
+app.use(express.logger('dev'));
+app.use('/', express.static(__dirname + '/../client'));
