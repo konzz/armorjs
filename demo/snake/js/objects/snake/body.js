aba1386b0b634de549a871ae8e4c2432badcd798
konzz <acasadotorres@gmail.com>
2015-03-19 00:24:42 +0000

    fixed bug drawing snake body

--- demo/snake/js/objects/snake/body.js
+++ demo/snake/js/objects/snake/body.js
@@ -8,14 +8,14 @@ define(function(){
       update: function(){
         body.position.x++;
         draw();
-
       }
     }
 
     function draw() {
+      body.ctx.beginPath();
       body.ctx.rect(body.position.x, body.position.y, 20, 10);
       body.ctx.fill();
-      body.ctx.stroke();
+      body.ctx.closePath();
     }
 
     return body;
