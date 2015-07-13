849a95747bca2e885d98dc0dc20bd79434520ba4
konzz <acasadotorres@gmail.com>
2015-07-13 01:40:54 +0100

    added substract method to v2

--- engine/vector2.js
+++ engine/vector2.js
@@ -1,6 +1,5 @@
-'use strict';
-
 define(['underscore'], function(_){
+  'use strict';
   var v2 = {
     add: function(a, b){
       if(b){
@@ -9,6 +8,15 @@ define(['underscore'], function(_){
       this.x += a.x;
       this.y += a.y;
     },
+
+    substract: function(a, b){
+      if(b){
+        return this.new(a.x - b.x, a.y - b.y);
+      }
+      this.x -= a.x;
+      this.y -= a.y;
+    },
+
     new: function(x, y){
       var v = _.clone(v2);
       v.x = x;
--- specs/vector2.spec.js
+++ specs/vector2.spec.js
@@ -1,6 +1,5 @@
-'use strict';
-
 define(['engine/vector2'], function(v2){
+  'use strict';
   describe('vector2', function(){
 
     describe('new()', function(){
@@ -18,9 +17,9 @@ define(['engine/vector2'], function(v2){
 
     describe('add()', function(){
       it('should return a vector resulting of adding the values of two vectors', function(){
-        var sum = v2.add({x:1, y:0}, {x:2, y:1});
-        expect(sum.x).toBe(3);
-        expect(sum.y).toBe(1);
+        var result = v2.add({x:1, y:0}, {x:2, y:1});
+        expect(result.x).toBe(3);
+        expect(result.y).toBe(1);
       });
 
       describe('when only one object passed', function(){
@@ -33,6 +32,22 @@ define(['engine/vector2'], function(v2){
       });
     });
 
+    describe('substract()', function(){
+      it('should return a vector resulting of substracting the values of two vectors', function(){
+        var result = v2.substract({x:3, y:2}, {x:2, y:1});
+        expect(result.x).toBe(1);
+        expect(result.y).toBe(1);
+      });
+
+      describe('when only one object passed', function(){
+        it('should substract to itself', function(){
+          var position = v2.new(10, 8);
+          position.substract({x: 5, y: 5});
+          expect(position.x).toBe(5);
+          expect(position.y).toBe(3);
+        });
+      });
+    });
 
   });
 });
