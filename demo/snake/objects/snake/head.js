21202c6da763570c35a95f6e4d7a1cc290af0934
konzz <acasadotorres@gmail.com>
2015-07-20 16:24:25 +0100

    snake dies when collides with himself

--- demo/snake/objects/snake/head.js
+++ demo/snake/objects/snake/head.js
@@ -1,4 +1,4 @@
-define(['engine/engine', 'jquery'], function(engine, $){
+define(['engine/engine', 'underscore'], function(engine, _){
   'use strict';
   return function(){
 
@@ -8,13 +8,35 @@ define(['engine/engine', 'jquery'], function(engine, $){
     var head = {
       moveRate: 100,
       color: "#DB303C",
+      alive: true,
 
       update: function(){
+        if(!head.alive){
+          return;
+        }
+
         if(lastMovement + head.moveRate <= Date.now()){
           head.gameObject.components.body.updatePosition();
-          move();
+          head.move();
         }
-        draw();
+
+        head.draw();
+        head.checkBodyCollisions();
+      },
+
+      checkBodyCollisions: function(){
+        var links = head.gameObject.components.body.links;
+        _(links).each(function(link){
+          var sameX = link.position.x === head.gameObject.position.x;
+          var sameY = link.position.y === head.gameObject.position.y;
+          if(sameX && sameY){
+            head.die();
+          }
+        });
+      },
+
+      die: function(){
+        head.alive = false;
       },
 
       turnRight: function(direction){
@@ -33,28 +55,29 @@ define(['engine/engine', 'jquery'], function(engine, $){
         head.velocity.x = y;
         head.velocity.y = x;
         canTurn = false;
+      },
+
+      move: function(){
+        if(!head.velocity){
+          head.velocity = engine.v2.new(head.gameObject.width, 0);
+        }
+
+        head.gameObject.position.add(head.velocity);
+        lastMovement =  Date.now();
+        canTurn = true;
+      },
+
+      draw: function() {
+        var position = head.gameObject.position;
+        head.ctx.beginPath();
+        head.ctx.rect(position.x, position.y, head.gameObject.width, head.gameObject.height);
+        head.ctx.fillStyle = head.color;
+        head.ctx.fill();
+        head.ctx.closePath();
+        head.ctx.stroke();
       }
+
     };
-
-    function move(){
-      if(!head.velocity){
-        head.velocity = engine.v2.new(head.gameObject.width, 0);
-      }
-
-      head.gameObject.position.add(head.velocity);
-      lastMovement =  Date.now();
-      canTurn = true;
-    }
-
-    function draw() {
-      var position = head.gameObject.position;
-      head.ctx.beginPath();
-      head.ctx.rect(position.x, position.y, head.gameObject.width, head.gameObject.height);
-      head.ctx.fillStyle = head.color;
-      head.ctx.fill();
-      head.ctx.closePath();
-      head.ctx.stroke();
-    }
 
     return head;
   };
--- demo/snake/objects/snake/specs/snake.head.spec.js
+++ demo/snake/objects/snake/specs/snake.head.spec.js
@@ -2,20 +2,20 @@ define(['../snake', 'engine/engine', 'engine/mocks'], function(snake, engine, mo
   'use strict';
   describe('snake head', function(){
 
-    var snakeHead;
+    var head;
     var timeSpy;
     var snakeObject;
     beforeEach(function(){
       snakeObject = snake(25,10);
       timeSpy = spyOn(Date, 'now').and.returnValue(0);
-      snakeHead = snakeObject.components.head;
-      snakeHead.ctx = mocks.ctx;
+      head = snakeObject.components.head;
+      head.ctx = mocks.ctx;
     });
 
     describe('update', function(){
       it('should draw the head of the snake', function(){
         spyOn(mocks.ctx, 'rect');
-        snakeHead.update();
+        head.update();
 
         expect(mocks.ctx.rect)
         .toHaveBeenCalledWith(
@@ -27,39 +27,87 @@ define(['../snake', 'engine/engine', 'engine/mocks'], function(snake, engine, mo
       });
 
       it('should move the head if enought time has past since last movement', function(){
-        snakeHead.moveRate = 100;
+        head.moveRate = 100;
         expect(snakeObject.position.x).toBe(25);
 
         timeSpy.and.returnValue(100);
-        snakeHead.update();
+        head.update();
         expect(snakeObject.position.x).toBe(35);
 
         timeSpy.and.returnValue(199);
-        snakeHead.update();
+        head.update();
         expect(snakeObject.position.x).toBe(35);
 
         timeSpy.and.returnValue(200);
-        snakeHead.update();
+        head.update();
         expect(snakeObject.position.x).toBe(45);
+      });
+
+      it('should check for body colisions', function(){
+        spyOn(head, 'checkBodyCollisions');
+        head.update();
+        expect(head.checkBodyCollisions).toHaveBeenCalled();
+      });
+    });
+
+    describe('checkBodyCollisions()', function(){
+      describe('when the head hits the body', function(){
+        it('should call die()', function(){
+          var position = engine.v2.new(10, 10);
+          snakeObject.components.body.addLink();
+          spyOn(head, 'die');
+
+          snakeObject.position = position;
+          snakeObject.components.body.links[0].position = position;
+
+          head.checkBodyCollisions();
+
+          expect(head.die).toHaveBeenCalled();
+        });
+      });
+
+      describe('when the head does not collide with the body', function(){
+        it('should not call die()', function(){
+          snakeObject.components.body.addLink();
+          snakeObject.position.x = 100;
+          spyOn(head, 'die');
+
+          head.checkBodyCollisions();
+
+          expect(head.die).not.toHaveBeenCalled();
+        });
+      });
+    });
+
+    describe('die()', function(){
+      it('stops the snake', function(){
+        spyOn(snakeObject.components.body, 'updatePosition');
+        spyOn(head, 'move');
+        head.moveRate = 0;
+        head.die();
+        head.update();
+
+        expect(head.move).not.toHaveBeenCalled();
+        expect(snakeObject.components.body.updatePosition).not.toHaveBeenCalled();
       });
     });
 
     describe('turning', function(){
       describe('turnLeft()', function(){
         it('should change the snake head direction to the left', function(){
-          snakeHead.velocity = engine.v2.new(10, 0);
-          snakeHead.turnLeft();
-          expect(snakeHead.velocity.x).toBe(0);
-          expect(snakeHead.velocity.y).toBe(-10);
+          head.velocity = engine.v2.new(10, 0);
+          head.turnLeft();
+          expect(head.velocity.x).toBe(0);
+          expect(head.velocity.y).toBe(-10);
         });
       });
 
       describe('turnRight()', function(){
         it('should change the snake head direction to the left', function(){
-          snakeHead.velocity = engine.v2.new(10, 0);
-          snakeHead.turnRight();
-          expect(snakeHead.velocity.x).toBe(0);
-          expect(snakeHead.velocity.y).toBe(10);
+          head.velocity = engine.v2.new(10, 0);
+          head.turnRight();
+          expect(head.velocity.x).toBe(0);
+          expect(head.velocity.y).toBe(10);
         });
       });
     });
